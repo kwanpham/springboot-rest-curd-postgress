dd9d7e8466a5f10472879203cdc816478955313c
quandev <chomchomcapital@gmail.com>
2020-10-13 17:29:15 +0700

    update

--- src/main/java/com/quandev/springbootrestcurdpostgress/controller/IndexController.java
+++ src/main/java/com/quandev/springbootrestcurdpostgress/controller/IndexController.java
@@ -4,6 +4,8 @@ import com.quandev.springbootrestcurdpostgress.model.Tutorial;
 import com.quandev.springbootrestcurdpostgress.model.TutorialEnum;
 import com.quandev.springbootrestcurdpostgress.model.TutorialReq;
 import com.quandev.springbootrestcurdpostgress.repository.TutorialRepository;
+import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.EnumUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -11,11 +13,15 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
+@Slf4j
 @Controller
 public class IndexController {
 
@@ -31,12 +37,26 @@ public class IndexController {
     @ResponseBody
     @PostMapping("/search")
     public ResponseEntity searchPage(@RequestBody TutorialReq req) {
-        System.out.println(req);
+        log.info(req.toString());
         Page<Tutorial> page= tr.findDysnamic(req.getTitle() , req.getDescription() , req.getPublished() , EnumUtils.getEnum(TutorialEnum.class,req.getStatus()) , PageRequest.of(req.getPage() , req.getSize()));
         if (page.isEmpty()){
             return ResponseEntity.noContent().build();
         } else
             return ResponseEntity.ok(page);
     }
+
+    @ResponseBody
+    @DeleteMapping("/delete")
+    public ResponseEntity deleteMutil(@RequestBody List<Long> ids) {
+        log.info(ids.toString());
+        if (ids.isEmpty()){
+            return ResponseEntity.badRequest().build();
+        } else {
+            ids.forEach(id -> tr.deleteToturial(TutorialEnum.INACTIVE , id));
+        }
+        return ResponseEntity.ok().build();
+
+    }
+
 
 }
